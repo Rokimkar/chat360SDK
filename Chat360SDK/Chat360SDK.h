ca31d46351597793244fc490423503ed45ce504c
Sanchit Singh <sanchit.singh@gaana.com>
2022-03-10 00:06:51 +0530

    Initial Commit

---
+++ Chat360SDK/Chat360SDK.h
@@ -0,0 +1,18 @@
+//
+//  Chat360SDK.h
+//  Chat360SDK
+//
+//  Created by Sanchit Kumar Singh on 10/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Chat360SDK.
+FOUNDATION_EXPORT double Chat360SDKVersionNumber;
+
+//! Project version string for Chat360SDK.
+FOUNDATION_EXPORT const unsigned char Chat360SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chat360SDK/PublicHeader.h>
+
+
