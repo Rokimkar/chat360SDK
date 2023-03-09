51433ce0323f1af886d1758712e1bc4a720fa18f
Sanchit Kumar Singh <sanchitkumarsingh@gmail.com>
2023-03-09 18:42:22 +0530

    minor changes

--- Chat360Example/ChatViewController.swift
+++ Chat360Example/ChatViewController.swift
@@ -12,7 +12,6 @@ class ChatViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func launchTapped(_ sender: Any) {
